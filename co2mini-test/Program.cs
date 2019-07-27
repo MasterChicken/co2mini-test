55969696b88e9bd6cc4deec0eef8d0dcdbe2f96a
MasterChicken <hansyou@gmail.com>
2019-07-27 13:12:09 +0900

    取得結果をJSONで返すようにした

--- co2mini-test/Program.cs
+++ co2mini-test/Program.cs
@@ -4,29 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using static System.Console;
 using HidLibrary;
 
-namespace co2mini_test
+namespace Co2miniTest
 {
-    public class DataDecryptionException : Exception
-    {
-        public DataDecryptionException() : base() { }
-        public DataDecryptionException(string message) : base(message) { }
-    }
-
-
     public static class Program
     {
         /// <summary>ベンダーID</summary>
         private static readonly int CO2MINI_VENDOR_ID = 0x04d9;
         /// <summary>プロダクトID</summary>
         private static readonly int CO2MINI_PRODUCT_ID = 0xa052;
-        /// <summary>初期化＆解読時に使用するマジックナンバー</summary>
-        private static readonly byte[] MAGIC_NUMBERS_1 = new byte[] { 0xC4, 0xC6, 0xC0, 0x92, 0x40, 0x23, 0xDC, 0x96 };
+        /// <summary>初期化に使用するマジックナンバー</summary>
+        private static readonly byte[] INIT_MAGIC_NUMBERS = new byte[] { 0x00, 0xC4, 0xC6, 0xC0, 0x92, 0x40, 0x23, 0xDC, 0x96 };
+        /// <summary>解読時に使用するマジックナンバー1</summary>
+        private static readonly int[] DECRYPT_MAGIC_NUMBERS_1 = new int[] { 0xC4, 0xC6, 0xC0, 0x92, 0x40, 0x23, 0xDC, 0x96 };
         /// <summary>解読時に使用するマジックナンバー2</summary>
-        private static readonly int[] MAGIC_NUMBERS_2 = new int[] { 0x48, 0x74, 0x65, 0x6D, 0x70, 0x39, 0x39, 0x65 };
+        private static readonly int[] DECRYPT_MAGIC_NUMBERS_2 = new int[] { 0x48, 0x74, 0x65, 0x6D, 0x70, 0x39, 0x39, 0x65 };
         /// <summary>解読時に使用するマジックナンバー3</summary>
-        private static readonly int[] MAGIC_NUMBERS_3 = new int[] { 0x84, 0x47, 0x56, 0xD6, 0x07, 0x93, 0x93, 0x56 };
+        private static readonly int[] DECRYPT_MAGIC_NUMBERS_3 = new int[] { 0x84, 0x47, 0x56, 0xD6, 0x07, 0x93, 0x93, 0x56 };
         /// <summary>解読時に使用するデータのシャッフル順</summary>
         private static readonly int[] DATA_SHUFFLE_ORDER = new int[] { 2, 4, 0, 7, 1, 6, 5, 3 };
         /// <summary>データの既定の長さ</summary>
@@ -39,10 +35,179 @@ namespace co2mini_test
         private enum ReadDataType
         {
             None,
-            CO2,
+            Co2,
             Temperature
         }
 
+        /// <summary>
+        /// 処理対象のデバイス
+        /// </summary>
+        public static HidDevice device = null;
+
+        /// <summary>
+        /// データの収集完了を待つシグナル
+        /// </summary>
+        public static ManualResetEventSlim dataCollectedSignal = null;
+
+        /// <summary>
+        /// すでに終了処理が始まっているかどうか
+        /// </summary>
+        public static bool quitting = false;
+
+        /// <summary>
+        /// CO2濃度（PPM）
+        /// </summary>
+        public static int? co2 = null;
+
+        /// <summary>
+        /// 気温（セルシウス度）
+        /// </summary>
+        public static double? temperature = null;
+
+        public static void Main()
+        {
+            // 例外処理定義
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
+            try
+            {
+                // デバイスを検索
+                var devices = HidDevices.Enumerate(CO2MINI_VENDOR_ID, new int[] { CO2MINI_PRODUCT_ID });
+
+                if (!devices.Any())
+                {
+                    throw new Exception("no co2mini device found");
+                }
+                if (devices.Count() != 1)
+                {
+                    throw new Exception("multiple co2mini device found");
+                }
+
+                device = devices.First();
+                device.MonitorDeviceEvents = true;
+                device.OpenDevice();
+
+                 // データ収集待ちシグナル
+                dataCollectedSignal = new ManualResetEventSlim();
+
+                if (!device.IsOpen || !device.IsConnected)
+                {
+                    throw new Exception("device is neither opened nor connected");
+                }
+
+                // 初期化要求送信
+                if (!device.WriteFeatureData(INIT_MAGIC_NUMBERS))
+                {
+                    throw new Exception("failed to write feature report while initialization");
+                }
+
+                // データ読み取り
+                device.ReadReport(DeviceOnReport, Convert.ToInt32(DATA_TIMEOUT.TotalMilliseconds));
+                dataCollectedSignal.Wait();
+
+                // 結果表示
+                var resultData = new ResultData
+                {
+                    IsOK = true,
+                    Co2 = co2,
+                    Temperature = temperature
+                };
+                Write(resultData.ToJson());
+
+            }
+            finally
+            {
+                quitting = true;
+
+                // シグナルを止める
+                if (dataCollectedSignal != null)
+                {
+                    dataCollectedSignal.Dispose();
+                }
+                dataCollectedSignal = null;
+
+                // デバイスを閉じる
+                if (device != null)
+                {
+                    device.CloseDevice();
+                    device.Dispose();
+                }
+                device = null;
+            }
+
+
+#if DEBUG
+            ReadLine();
+#endif
+
+            return;
+        }
+
+        /// <summary>
+        /// デバイスから送られたレポートを解析します。
+        /// </summary>
+        /// <param name="report"></param>
+        public static void DeviceOnReport(HidReport report)
+        {
+            // 終了処理中
+            if (quitting)
+            {
+                return;
+            }
+
+            // デバイス存在確認
+            if (device == null || !device.IsOpen || !device.IsConnected)
+            {
+                goto fail;
+            }
+
+            // レポート存在確認
+            if (report == null || !report.Exists)
+            {
+                goto fail;
+            }
+
+            // レポート読み取り
+            var rawData = report.Data;
+            if (rawData == null || rawData.Length != DATA_LENGTH)
+            {
+                goto fail;
+            }
+
+            try
+            {
+                var decryptedData = DecryptData(rawData);
+                var (type, value) = ReadData(decryptedData);
+
+                switch (type)
+                {
+                    case ReadDataType.Co2:
+                        co2 = Convert.ToInt32(value);
+                        break;
+                    case ReadDataType.Temperature:
+                        temperature = Math.Round(value * 10000) / 10000;
+                        break;
+                    default:
+                        throw new NotImplementedException();
+                }
+
+            }
+            // 無視するエラー
+            catch (UnsupportedDataException) { }
+            catch (DataDecryptionException) { }
+
+        fail:
+            if (co2.HasValue && temperature.HasValue)
+            {
+                // データ収集完了
+                dataCollectedSignal.Set();
+            }
+            else if (device != null && device.IsOpen && device.IsConnected)
+            {
+                // 未完了につき、まだ待つ
+                device.ReadReport(DeviceOnReport, Convert.ToInt32(DATA_TIMEOUT.TotalMilliseconds));
+            }
+        }
 
         /// <summary>
         /// 受信したデータの暗号化を解除し、破損していないデータを返します。
@@ -51,9 +216,6 @@ namespace co2mini_test
         /// <returns></returns>
         static int[] DecryptData(byte[] data)
         {
-            if (data == null) { throw new ArgumentNullException(nameof(data)); }
-            if (data.Length != DATA_LENGTH) { throw new ArgumentException($"{nameof(data)}.Length != {DATA_LENGTH}"); }
-
             var p1 = new int[DATA_LENGTH];
             var p2 = new int[DATA_LENGTH];
             var p3 = new int[DATA_LENGTH];
@@ -68,7 +230,7 @@ namespace co2mini_test
             // XOR
             for (var i = 0; i < DATA_LENGTH; ++i)
             {
-                p2[i] = p1[i] ^ MAGIC_NUMBERS_1[i];
+                p2[i] = p1[i] ^ DECRYPT_MAGIC_NUMBERS_1[i];
             }
 
             // ビットシフト
@@ -80,7 +242,7 @@ namespace co2mini_test
             // 差し引き
             for (var i = 0; i < DATA_LENGTH; ++i)
             {
-                p4[i] = ((0x100 + p3[i] - MAGIC_NUMBERS_3[i]) & 0xff);
+                p4[i] = ((0x100 + p3[i] - DECRYPT_MAGIC_NUMBERS_3[i]) & 0xff);
             }
 
             // チェックサム
@@ -99,16 +261,13 @@ namespace co2mini_test
         /// <returns></returns>
         static (ReadDataType type, double value) ReadData(int[] data)
         {
-            if (data == null) { throw new ArgumentNullException(nameof(data)); }
-            if (data.Length != DATA_LENGTH) { throw new ArgumentException($"{nameof(data)}.Length != {DATA_LENGTH}"); }
-
             var op = data[0];
             var value = data[1] << 8 | data[2];
 
             if (op == 0x50)
             {
                 // co2
-                return (ReadDataType.CO2, value);
+                return (ReadDataType.Co2, value);
             }
             else if (op == 0x42)
             {
@@ -117,155 +276,76 @@ namespace co2mini_test
             }
             else
             {
-                // none
-                return (ReadDataType.None, 0);
+                // サポートされていないデータ
+                throw new UnsupportedDataException();
             }
         }
 
-        static int Main()
+
+
+        /// <summary>
+        /// 例外エラー処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
         {
-            try
+            quitting = true;
+
+            var resultData = new ResultData
             {
-                // デバイスを検索
-                var devices = HidDevices.Enumerate(CO2MINI_VENDOR_ID, new int[] { CO2MINI_PRODUCT_ID });
-
-                if (!devices.Any()) { throw new Exception("no valid device found"); }
-                if (devices.Count() != 1) { throw new Exception("multiple devices currently not supported"); }
-
-                // 今のところ最初の1台にだけ対応
-                var device = devices.First();
-                int? valueCO2 = null;
-                double? valueTemp = null;
-
-                CountdownEvent waitDataSignal = null;
-                System.Timers.Timer timer = null;
-                DateTime startTime = DateTime.MinValue;
-
-                device.MonitorDeviceEvents = true;
-                device.OpenDevice();
-                try
-                {
-                    if (!device.WriteFeatureData(MAGIC_NUMBERS_1.Prepend((byte)0x0).ToArray()))
-                    {
-                        throw new Exception("writing feature report failed");
-                    }
-
-                    // データが来た際のコールバック
-                    void OnReport(HidReport report)
-                    {
-                        if (!device.IsOpen || !device.IsConnected || waitDataSignal.IsSet) { return; }
-
-                        if (report.Exists)
-                        {
-                            var rawBytes = report.Data;
-                            if (rawBytes != null && rawBytes.Length == DATA_LENGTH)
-                            {
-                                try
-                                {
-                                    var rawData = DecryptData(rawBytes);
-                                    var (type, value) = ReadData(rawData);
-
-                                    if (type == ReadDataType.CO2)
-                                    {
-                                        valueCO2 = (int)value;
-                                    }
-                                    else if (type == ReadDataType.Temperature)
-                                    {
-                                        valueTemp = value;
-                                    }
-                                }
-                                catch (DataDecryptionException) { }
-                            }
-                        }
-
-                        if (valueCO2.HasValue && valueTemp.HasValue)
-                        {
-                            // データが揃った
-                            waitDataSignal.Signal();
-                        }
-                        else
-                        {
-                            // まだ待つ
-                            device.ReadReport(OnReport);
-                        }
-                    }
-
-                    waitDataSignal = new CountdownEvent(1);
-
-                    timer = new System.Timers.Timer()
-                    {
-                        Enabled = false,
-                        AutoReset = false,
-                        Interval = 1000,
-                    };
-                    timer.Elapsed += (sender, e) =>
-                    {
-                        timer.Enabled = false;
-                        if (waitDataSignal.IsSet) { return; }
-
-                        if ((e.SignalTime - startTime) > DATA_TIMEOUT)
-                        {
-                            // 時間切れ
-                            waitDataSignal.Signal();
-                        }
-                        else
-                        {
-                            // まだ待つ
-                            timer.Enabled = true;
-                        }
-                    };
-
-                    startTime = DateTime.Now;
-                    timer.Enabled = true;
-                    device.ReadReport(OnReport);
-
-                    waitDataSignal.Wait();
-                    timer.Enabled = false;
-
-
-                }
-                finally
-                {
-                    device.CloseDevice();
-                }
-
-                // 結果を書き出す
-                Console.Write("co2:");
-                if (valueCO2.HasValue)
-                {
-                    Console.Write(valueCO2.Value.ToString("0", INVALIANT_CULTURE));
-                }
-                else
-                {
-                    Console.Write("null");
-                }
-                Console.Write("\t");
-
-                Console.Write("temp:");
-                if (valueTemp.HasValue)
-                {
-                    Console.Write(valueTemp.Value.ToString("0.0#####", INVALIANT_CULTURE));
-                }
-                else
-                {
-                    Console.Write("null");
-                }
-                Console.Write("\t");
-                Console.WriteLine();
-
-#if DEBUG
-                Console.ReadKey();
-#endif
-
-                return 0;
-
-            }
-            catch (Exception ex)
+                IsOK = false,
+            };
+            if (e.ExceptionObject != null)
             {
-                Console.Error.WriteLine(ex.ToString());
-                return 1;
+                resultData.Message = e.ExceptionObject.ToString();
             }
+            else
+            {
+                resultData.Message = "unknown error occured";
+            }
+            Write(resultData.ToJson());
 
+            // シグナルを止める
+            if (dataCollectedSignal != null)
+            {
+                dataCollectedSignal.Dispose();
+            }
+            dataCollectedSignal = null;
+
+            // デバイスを閉じる
+            if (device != null)
+            {
+                device.CloseDevice();
+                device.Dispose();
+            }
+            device = null;
+
+            // エラー終了
+            Environment.Exit(1);
         }
+    }
+
+    [Serializable]
+    public class DataDecryptionException : Exception
+    {
+        public DataDecryptionException() { }
+        public DataDecryptionException(string message) : base(message) { }
+        public DataDecryptionException(string message, Exception inner) : base(message, inner) { }
+        protected DataDecryptionException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+
+
+    [Serializable]
+    public class UnsupportedDataException : Exception
+    {
+        public UnsupportedDataException() { }
+        public UnsupportedDataException(string message) : base(message) { }
+        public UnsupportedDataException(string message, Exception inner) : base(message, inner) { }
+        protected UnsupportedDataException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
     }
 }
---
+++ co2mini-test/ResultData.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.IO;
+
+namespace Co2miniTest
+{
+    /// <summary>
+    /// 取得結果をセットするオブジェクト
+    /// </summary>
+    [DataContract]
+    public class ResultData
+    {
+        /// <summary>
+        /// 取得に成功したかどうか
+        /// </summary>
+        [DataMember(EmitDefaultValue = true, IsRequired = true, Name = "isOk")]
+        public bool IsOK { get; set; } = false;
+
+        /// <summary>
+        /// 取得に際して発生したメッセージ
+        /// </summary>
+        [DataMember(EmitDefaultValue = true, Name = "message")]
+        public string Message { get; set; } = null;
+
+        /// <summary>
+        /// 二酸化炭素濃度（PPM）
+        /// </summary>
+        [DataMember(EmitDefaultValue = false, Name = "co2")]
+        public int? Co2 { get; set; } = null;
+
+        /// <summary>
+        /// 気温（セルシウス度）
+        /// </summary>
+        [DataMember(EmitDefaultValue = false, Name = "temperature")]
+        public double? Temperature { get; set; } = null;
+
+        /// <summary>
+        /// 現在のオブジェクト意をJSON文字列にして返します。
+        /// </summary>
+        /// <returns></returns>
+        public string ToJson()
+        {
+            using (var memoryStream = new MemoryStream())
+            using (var streamReader = new StreamReader(memoryStream))
+            {
+                var serializer = new DataContractJsonSerializer(typeof(ResultData));
+                serializer.WriteObject(memoryStream, this);
+                memoryStream.Position = 0;
+
+                return streamReader.ReadToEnd();
+            }
+        }
+    }
+}
